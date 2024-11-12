888d8d9fbc224eea289d25d72f40c7a8b572c0a5
Stepan Maliarovskii <smalyarovsky@gmail.com>
2024-11-12 14:16:38 +0200

    Added pseudo-entities of to be added Supply class. Added list and get paths.

--- app/client.py
+++ app/client.py
@@ -1,14 +1,15 @@
 import requests
 
+
 def check_root_url(url):
     try:
         response = requests.get(url)
-        if response.status_code == 400:
+        if response.status == 200:
             print("Запрос успешен, код 200.")
         else:
-            exit(-1)
+            exit(1)
     except requests.RequestException as e:
-        exit(-1)
+        exit(1)
 
 
 check_root_url("http://localhost:8080")
--- app/server.py
+++ app/server.py
@@ -1,9 +1,76 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# TODO implement as a class
+supply_examples = [
+    {
+        "name": "milk",
+        "divisible": True,
+        "amount": 1000,
+        "units": "mL",
+        "calories": 420,
+        "proteins": 10.0,
+        "fats": 34.0,
+        "carbohydrates": 50.0,
+        "price": 1.55
+    },
+    {
+        "name": "snickers",
+        "divisible": False,
+        "calories": 488,
+        "proteins": 7.0,
+        "fats": 24.0,
+        "carbohydrates": 73.0,
+        "price": 1.1
+    }
+]
+
+
+@app.route("/list")
+def list():
+    message = {
+        "status": 200,
+        "message": "OK",
+        "list": supply_examples
+    }
+    return jsonify(message)
+
+
+@app.route("/add")
+def add():
+    supply = dict()
+    try:
+        supply["name"] = request.args.get("name")
+        divisible = request.args.get("divisible")
+        assert(divisible == "true" or divisible == "false")
+        supply["divisible"] = (divisible == "true")
+        if supply["divisible"]:
+            supply["amount"] = int(request.args.get("amount"))
+            supply["units"] = int(request.args.get("units"))
+        supply["calories"] = int(request.args.get("units"))
+        supply["proteins"] = float(request.args.get("units"))
+        supply["fats"] = float(request.args.get("units"))
+        supply["carbohydrates"] = float(request.args.get("units"))
+        supply["price"] = float(request.args.get("units"))
+    except Exception as e:
+        message = {
+            "status": 400,
+            "message": "Invalid arguments"
+        }
+        return jsonify(message)
+    message = {
+        "status": 200,
+        "message": "OK"
+    }
+    return jsonify(message)
+
+
+app.run(debug=True, port=8080)
